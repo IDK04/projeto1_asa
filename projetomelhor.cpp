1a2b49b4dcb9f14ffc76f06bc5d9e59621abf1d4
Duarte Laia <duarte.laia04@gmail.com>
2023-11-20 22:39:43 +0000

    a

--- projetomelhor.cpp
+++ projetomelhor.cpp
@@ -20,6 +20,7 @@ typedef struct {
     int col;
 } part;
 
+/* cut */
 typedef struct {
     part p;
     int price;
@@ -35,37 +36,36 @@ part transpose(part p){
 vector<cut> readInput(){
     int n;
     cin >> n;
-    vector<cut> cuts;
+    vector<cut> cuts ;
 
     for (int i=0;i<n;i++){
         cut c,cT;
         cin >> c.p.line >> c.p.col >> c.price;
         cT.p = transpose(c.p);
+        cT.price = c.price;
         cuts.push_back(c);
         cuts.push_back(cT);
     }
     return cuts;
 }
 
-int knapsack(vector<cut> cuts,part maxSize) {
+int knapsackv2(vector<cut> cuts,part maxSize) {
     vector<vector<int>> values(maxSize.line+1, vector<int>(maxSize.col+1, 0));
     int numCuts = cuts.size();
     for (int i = 1; i <= maxSize.line; i++) {
         for(int j = 1; j <= maxSize.col; j++) {
             for (int w = 0; w < numCuts; w++) {
-                part currentPart = cuts[w].p;
-                if (currentPart.line <=  i && currentPart.col <= j){
+                part p1=cuts[w].p;
+                if (p1.line <=  i && p1.col <= j){
                     int final = max (cuts[w].price,
-                                    max(values[i][currentPart.col]+values[i][j-currentPart.col],
-                                    values[currentPart.line][j]+values[i-currentPart.line][j]));
-                    
+                                    max(values[i][p1.col]+values[i][j-p1.col],
+                                        values[p1.line][j]+values[i-p1.line][j]));
+
                     values[i][j] = max(values[i][j],final);
                 }
             }
         }
-    }
-
-    return values[maxSize.line][maxSize.col];
+    } return values[maxSize.line][maxSize.col];
 }
 
 int main(){
@@ -74,7 +74,7 @@ int main(){
     int x,y;
     cin >> x >> y;
     vector<cut> cuts = readInput();
-    int result = knapsack(cuts,{x,y});
+    int result = knapsackv2(cuts,{x,y});
     cout << result << "\n";
     auto end = std::chrono::high_resolution_clock::now();
 
